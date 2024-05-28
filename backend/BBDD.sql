0aabd00bcc1a61570b531ae29ff7959fac576e83
FabioChP <145000582+FabioChP@users.noreply.github.com>
2024-05-28 19:42:15 +0200

    Arreglado el backend y creado la view crear_usuario

--- backend/BBDD.sql
+++ backend/BBDD.sql
@@ -2,27 +2,22 @@ DROP DATABASE IF EXISTS urlshortener;
 CREATE DATABASE urlshortener;
 USE urlshortener;
 
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+	id int primary key not null auto_increment,
+    uname varchar(50) not null,
+    email varchar(100) not null,
+    passwd varchar(100) not null
+);
+
+
 DROP TABLE IF EXISTS urls;
 CREATE TABLE urls(
 	id int primary key not null auto_increment,
 	old_route varchar(200) not null,
 	new_route varchar(100) not null,
 	clicks int not null default 0,
-    fcreacion datetime not null
-);
-
-DROP TABLE IF EXISTS users;
-CREATE TABLE users(
-	id int primary key not null auto_increment,
-    uname varchar(50) not null,
-    passwd varchar(100) not null
-);
-
-DROP TABLE IF EXISTS urlsPorUser;
-CREATE TABLE urlsPorUser(
-	id int primary key not null auto_increment,
-    url_id int not null,
-    user_id int not null,
-    CONSTRAINT FK_urls FOREIGN KEY (url_id) REFERENCES urls(id),
-    CONSTRAINT FK_username FOREIGN KEY (user_id) REFERENCES users(id)
+    fcreacion datetime not null,
+    user_creador int not null,
+    CONSTRAINT FK_urls_users FOREIGN KEY (user_creador) REFERENCES users(id)
 );
--- backend/urlShortener/webserviceapp/views.py
+++ backend/urlShortener/webserviceapp/views.py
@@ -1,15 +1,39 @@
-
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 
-
 # Create your views here.
 
-error_method = JsonResponse({"error":"Método incorrecto"}, status=405)
+error_method = JsonResponse({"error":"Método incorrecto"},status=405)
 
 def comprobar_url(request, url_comprobar):
     if request.method != "GET":
         return error_method
     urls = Urls.objects.get(url = url_comprobar)
     return JsonResponse ({"se_repite":len(urls)}, safe = False, json_dumps_params={'ensure_ascii': False})
+
+def crear_usuario(request):
+    return JsonResponse({"error":"No funcional todavía"},status=500)
+
+def devolver_usuario(request, nombre_user):
+    try:
+        usuario = Users.objects.get(uname = nombre_user)
+    
+        urls = usuario.urls_set.all()
+        
+        lista_urls = []
+        for fila in urls:
+            diccionario = {}
+            diccionario['old_route']
+            diccionario['new_route']
+            diccionario['clicks']
+            diccionario['fcreacion']
+            lista_urls.append(diccionario)
+        
+        respuesta = {
+            'nombre':usuario.uname,
+            'urls': lista_urls
+        }
+    except Users.DoesNotExist:
+        return JsonResponse({'error': 'El usuario no existe'}, status=405)
+    return JsonResponse(respuesta, safe=False, json_dumps_params={'ensure_ascii': False})
