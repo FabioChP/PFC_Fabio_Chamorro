50580d9b847240c5105d2c8080383acc87013b22
FabioChP <145000582+FabioChP@users.noreply.github.com>
2024-05-28 15:46:08 +0200

    Añadida la interfaz de la página

--- frontend/url-shortener/src/sites/login/login.js
+++ frontend/url-shortener/src/sites/login/login.js
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+import "./login.css"
+
 function Login() {
+
+    function a() {
+
+    }
 
     return <>
         <h1>LOGIN</h1>
+        <form className="login-form">
+            <input onChange={a} className="login-textInput" type="email" placeholder="Email"></input>
+            <input onChange={a} className="login-textInput" type="password" placeholder="Contraseña"></input>
+            <p className="login-formText">¿No tienes cuenta? Registrate <Link to="/register">aquí</Link>.</p>
+            <input onClick={a} className="login-button" type="submit" value="Login" />
+        </form>
     </>
 
 }
