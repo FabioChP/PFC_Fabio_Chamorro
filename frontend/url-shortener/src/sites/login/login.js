e969f17187846b5947f3ae0bc930b1a1a0f9171f
FabioChP <145000582+FabioChP@users.noreply.github.com>
2024-05-30 21:11:02 +0200

    Creadas las funciones onChange

--- frontend/url-shortener/src/sites/login/login.js
+++ frontend/url-shortener/src/sites/login/login.js
@@ -1,19 +1,30 @@
 import { Link } from "react-router-dom";
 import "./login.css"
+import { useState } from "react";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    function a() {
+    function onChangeEmail(event) {
+        setEmail(event.target.value)
+    }
+
+    function onChangePassword(event) {
+        setPassword(event.target.value)
+    }
+
+    function Publish(event) {
 
     }
 
     return <>
         <h1>LOGIN</h1>
         <form className="login-form">
-            <input onChange={a} className="login-textInput" type="email" placeholder="Email"></input>
-            <input onChange={a} className="login-textInput" type="password" placeholder="Contraseña"></input>
+            <input onChange={onChangeEmail} className="login-textInput" type="email" placeholder="Email"></input>
+            <input onChange={onChangePassword} className="login-textInput" type="password" placeholder="Contraseña"></input>
             <p className="login-formText">¿No tienes cuenta? Registrate <Link to="/register">aquí</Link>.</p>
-            <input onClick={a} className="login-button" type="submit" value="Login" />
+            <input onClick={Publish} className="login-button" type="submit" value="Login" />
         </form>
     </>
 
