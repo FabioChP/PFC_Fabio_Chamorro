e48469a9ef1dc3205c422adcb071006601fdf5cb
FabioChP <145000582+FabioChP@users.noreply.github.com>
2024-05-30 18:54:49 +0200

    Añadida función de crear usuarios

--- backend/urlShortener/webserviceapp/views.py
+++ backend/urlShortener/webserviceapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Users, Urls
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.hashers import check_password, make_password
+import json
 
 # Create your views here.
 
@@ -12,8 +15,25 @@ def comprobar_url(request, url_comprobar):
     urls = Urls.objects.get(url = url_comprobar)
     return JsonResponse ({"se_repite":len(urls)}, safe = False, json_dumps_params={'ensure_ascii': False})
 
+# --- GESTIÓN DE USUARIOS ---
+
+@csrf_exempt
 def crear_usuario(request):
-    return JsonResponse({"error":"No funcional todavía"},status=500)
+	if request.method != 'POST':
+		return error_method
+	data = json.loads(request.body)
+	try:
+		user=Users.objects.get(uname=data['username'])
+	except Users.DoesNotExist:
+		new_user = Users(
+			uname=data['username'],
+			email=data['email'],
+			passwd=make_password(data['password']),
+		)
+		new_user.save()
+		return JsonResponse({'message': 'Usuario registrado exitosamente'}, status=201)
+	else:
+		return JsonResponse({'error': 'Usuario existente'}, status=404)
 
 def devolver_usuario(request, nombre_user):
     try:
@@ -37,3 +57,5 @@ def devolver_usuario(request, nombre_user):
     except Users.DoesNotExist:
         return JsonResponse({'error': 'El usuario no existe'}, status=405)
     return JsonResponse(respuesta, safe=False, json_dumps_params={'ensure_ascii': False})
+
+# --- SIGUIENTE PUNTO ---
